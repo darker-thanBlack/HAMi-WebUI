9fdaec776facfaab51147246551056d7fd497fe4
m00960 <shentao199212@163.com>
2025-08-29 18:50:35 +0800

    support metax devices

---
+++ server/internal/provider/metax/device.go
@@ -0,0 +1,23 @@
+package metax
+
+import "vgpu/internal/provider/util"
+
+const (
+	DeviceAvaliable = 1
+
+	MetaxGPUDevice  = "Metax-GPU"
+	MetaxSGPUDevice = "Metax-SGPU"
+
+	MeteaxDevice  = "metax-tech.com/gpu"
+	MeteaxSDevice = "metax-tech.com/sgpu"
+
+	MetaxSDeviceAnno = "metax-tech.com/node-gpu-devices"
+)
+
+func init() {
+	util.InRequestDevices[MetaxGPUDevice] = "hami.io/metax-gpu-devices-to-allocate"
+	util.SupportDevices[MetaxGPUDevice] = "hami.io/metax-gpu-devices-allocated"
+
+	util.InRequestDevices[MetaxSGPUDevice] = "hami.io/metax-sgpu-devices-to-allocate"
+	util.SupportDevices[MetaxSGPUDevice] = "hami.io/metax-sgpu-devices-allocated"
+}
